01b879f5ca241154500c560975a25dd1e332c602
Max B <klmpfh@gmail.com>
2022-02-01 21:55:00 +0100

    send seperate

--- index_renderer.js
+++ index_renderer.js
@@ -2,7 +2,7 @@ const SerialPort = require('serialport')
 const Buffer = require('buffer').Buffer;
 
 // ls /dev/ttyUSB* /dev/ttyUSB0
-const port = new SerialPort('COM4', {
+const port = new SerialPort(process.platform == 'linux' ? '/dev/ttyUSB0' : 'COM4', {
   baudRate: 115200
 })
 
@@ -10,17 +10,14 @@ const buffer_size = 32*8*3;
 
 // sending some random stuff ...
 function sendSomeStuff(){
-  let buf = Buffer.allocUnsafe(buffer_size);
 
   for (let i = 0; i < buffer_size; i++) {
-    buf[i] = Math.floor(Math.random()*255);
+    port.write(Math.floor(Math.random()*255), function(err) {
+      if (err) {
+        return console.error('Error on write: ', err.message)
+      }
+    });
   }
-
-  port.write(buf.toString('hex'), function(err) {
-    if (err) {
-      return console.error('Error on write: ', err.message)
-    }
-  });
 
 }
 
