f62abf9a8763bb6ac8d655b7e4df6f94364f6492
Max B <klmpfh@gmail.com>
2022-02-01 21:42:56 +0100

    rm example :/

--- index_renderer.js
+++ index_renderer.js
@@ -5,12 +5,6 @@ const port = new SerialPort('COM4', {
   baudRate: 115200
 })
 
-port.write('main screen turn on', function(err) {
-  if (err) {
-    return console.log('Error on write: ', err.message)
-  }
-})
-
 const buffer_size = 32*8*3;
 
 // sending some random stuff ...
